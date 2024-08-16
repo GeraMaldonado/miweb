13a02fecd584cfb4a005c202c8bf8c287d21ecc0
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2024-08-16 17:44:10 -0600

    importacion de los componentes

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hola mundo</h1>
+  <Home/>
+  <Habilidades/>
+  <Proyectos/>
+  <Contacto/> 
+</template>
+<script setup>
+  import Home from '../components/home.vue'
+  import Habilidades from '../components/habilidades.vue'
+  import Proyectos from '../components/proyectos.vue'
+  import Contacto from '../components/contacto.vue'
+</script>
