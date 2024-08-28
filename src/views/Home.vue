420eb365bcefba8103b1a3d11c929baa2f5f947b
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2024-08-28 16:03:50 -0600

    Modificacion de vista para mostrar los componentes necesarios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,36 @@
 <template>
-  <h1>Hola mundo</h1>
-  <Home/>
-  <Habilidades/>
-  <Proyectos/>
-  <Contacto/> 
+  <Navegacion/>
+  <section id=inicio>
+    <Home/>
+  </section>
+  <section id=habilidades>
+    <Habilidades/>
+  </section>
+  <section id=proyectos>
+    <Proyectos/>
+  </section>
+  <section id=contacto>
+    <Contacto/>
+  </section>
 </template>
 <script setup>
+  import Navegacion from '../components/navegacion.vue'
   import Home from '../components/home.vue'
   import Habilidades from '../components/habilidades.vue'
   import Proyectos from '../components/proyectos.vue'
   import Contacto from '../components/contacto.vue'
 </script>
+<style scope>
+#inicio{
+  background-color: red;
+}
+#habilidades{
+  background-color: blue;
+}
+#proyectos{
+  background-color: yellow;
+}
+#contacto{
+  background-color: green;
+}
+</style>
