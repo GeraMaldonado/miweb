3ce27387eb954c64fe5f6d49e961a7802af7f2b7
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2024-08-28 16:03:02 -0600

    Modificacion de componente para mostrar toda la informacion relacionada

--- src/components/contacto.vue
+++ src/components/contacto.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Contacto</h1>
+  <div class=pantalla>
+    <div class=contenedorPantalla>
+      <div class=etiqueta>
+        <h1 class=titulo>Contacto</h1>
+      </div>
+      <div class=contacto>
+        <form>
+          <input type="text" id="nombre" name="nombre" placeholder="Nombre" required />
+          <input type="email" id="email" name="email" placeholder="Correo" required />
+          <textarea id="mensaje" name="menaje" placeholder="Mensaje" required />
+          <button type=button>Enviar</button>
+        </form>
+        <img class=logo src='../assets/contacto/github.png'>
+        <img class=logo src='../assets/contacto/linkedin.png'>
+        <img class=logo src='../assets/contacto/whatsapp.png'>
+      </div>
+    </div>
+  </div>
 </template>
