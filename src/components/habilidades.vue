0bc3b31e8b23e56aa978f03254761be0b2b67aee
GeraMaldonado <gmaldonadofelix@gmail.com>
2024-09-26 18:05:25 -0600

    Actualizacion de componente habilidades, mejorando vista

--- src/components/habilidades.vue
+++ src/components/habilidades.vue
@@ -4,19 +4,30 @@
       <div class="etiqueta">
         <h1 class="titulo">Habilidades</h1>
       </div>
-      <div class="contenedorFlex">
+      <div class="contenedorP">
         <img class="imagenHabilidad" src="../assets/1_tRlkqQL5xfgwHTFODp8LHw.webp" style="width: 40%;">
-        <div class="contenedorOscuro p">
+        <div class="contenedorOscuro">
           <div class="habilidades">
-            <img class="logo" src="../assets/habilidades/express.png">
-            <img class="logo" src="../assets/habilidades/mysql.png">
-            <img class="logo" src="../assets/habilidades/neovim.png">
-            <img class="logo" src="../assets/habilidades/node.png">
-            <img class="logo" src="../assets/habilidades/ubuntu.png">
-            <img class="logo" src="../assets/habilidades/vue.png">
+            <div class="contenedorLogo"><img class="logo" src="../assets/habilidades/express.png"></div>
+            <div class="contenedorLogo"><img class="logo" src="../assets/habilidades/mysql.png"></div>
+            <div class="contenedorLogo"><img class="logo" src="../assets/habilidades/neovim.png"></div>
+            <div class="contenedorLogo"><img class="logo" src="../assets/habilidades/node.png"></div>
+            <div class="contenedorLogo"><img class="logo" src="../assets/habilidades/ubuntu.png"></div>
+            <div class="contenedorLogo"><img class="logo" src="../assets/habilidades/vue.png"></div>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scope>
+.contenedorLogo{
+  will-change: filter;
+  transition: filter 300ms;
+}
+.contenedorLogo:hover{
+  filter: drop-shadow(0 0 2em #8280f0de)
+}
+
+</style>
