a3001d0db71e98f43029c6947deb7a56603f2ba1
GeraMaldonado <gmaldonadofelix@gmail.com>
2024-09-28 19:35:19 -0600

    Refactorizacion de estilos

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,44 @@
 <template>
-  <header>    
-  </header>
-  <Home/>
-  <footer>
-    <p>&copy Gerardo Maldonado. Derechos resesrvados</p>
-  </footer>
+  <body>
+    <Home/>
+    <footer>
+      <p>&copy Gerardo Maldonado. Derechos resesrvados</p>
+    </footer>
+  </body>
 </template>
 
 <script setup>
 import Home from './views/Home.vue'
+import '@fontsource/roboto';
 
 </script>
 <style>
 @import './assets/styles.css';
+@import './assets/reset.css';
+
+body {
+  font-family: 'Roboto', sans-serif;
+}
+
 footer{ 
   height: 10vh;
-  width: 100%;
+  width: 100vw;
   background-color: black;
-  color: white;
+  color: red;
   text-align: left;
-  padding-left: 30px;
-  bottom: 0;
- }
-/* http://meyerweb.com/eric/tools/css/reset/ 
-   v2.0 | 20110126
-   License: none (public domain)
-*/
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
+  bottom: 0;  
 }
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
+
+h1, h2, h3, h4 {
+  text-wrap: balance;
 }
-body {
-	line-height: 1;
+
+p {
+  text-wrap: pretty;
 }
-ol, ul {
-	list-style: none;
+
+header, footer, body {
+  min-width: 370px;
 }
-blockquote, q {
-	quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
-}
+
 </style>
