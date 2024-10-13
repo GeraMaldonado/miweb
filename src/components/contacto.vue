2428bb26b47dfdb8543c6d30efc9042768acd569
GeraMaldonado <gmaldonadofelix@gmail.com>
2024-10-13 16:37:39 -0600

    Se agregó la funcion para enviar correos

--- src/components/contacto.vue
+++ src/components/contacto.vue
@@ -1,27 +1,126 @@
 <template>
-  <div class=pantalla>
-    <div class=contenedorElementos>
-      <div class=etiqueta>
-          <h1 class=titulo>Contacto</h1>
-        </div>
-      <div class=contenedorOscuro>
-        <div class=contacto>
-          <form>
-            <input type="text" id="nombre" name="nombre" placeholder="Nombre" required />
-            <input type="email" id="email" name="email" placeholder="Correo" required />
-            <textarea id="mensaje" name="menaje" placeholder="Mensaje" required />
-            <button type=button>Enviar</button>
+  <div class="pantalla">
+    <div class="contenedorElementos">
+      <div class="etiqueta">
+        <h1 class="titulo">Contacto</h1>
+      </div>
+      <div class="contenedorOscuro">
+        <div class="contacto">
+          <form @submit.prevent="sendEmail">
+            <table>
+              <tbody class="contenidoContacto">
+                <tr class=apartadoContacto>
+<!--                  <td><label for="nombre">Nombre:</label></td>
+-->                  <td><input type="text" v-model="form.nombre" id="nombre" name="nombre" placeholder="Nombre" required /></td>
+                </tr>
+                <tr class=apartadoContacto>
+<!--                  <td><label for="email">Email:</label></td>
+-->                  <td><input type="email" v-model="form.email" id="email" name="email" placeholder="Correo" required /></td>
+                </tr>
+                <tr class=apartadoContacto>
+<!--                  <td><label for="mensaje">Mensaje:</label></td>
+-->                  <td><textarea v-model="form.mensaje" id="mensaje" name="mensaje" placeholder="Mensaje" required></textarea></td>
+                </tr>
+                <tr class=apartadoContacto>
+                  <td>
+                    <button type="submit">Enviar</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </form>
-          <img class=logoContacto src='../assets/contacto/github.png'>
-          <img class=logoContacto src='../assets/contacto/linkedin.png'>
-          <img class=logoContacto src='../assets/contacto/whatsapp.png'>
+          <div class="logosContactos">
+            <a href="https://github.com/GeraMaldonado"><img class="logoContacto" src="../assets/contacto/github.png" /></a>
+            <a href="https://www.linkedin.com/in/gerardo-maldonado-félix/"><img class="logoContacto" src="../assets/contacto/linkedin.png" /></a>
+            <a href="https://wa.me/+524921244858"><img class="logoContacto" src="../assets/contacto/whatsapp.png" /></a>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import emailjs from 'emailjs-com';
+import { reactive } from 'vue';
+
+
+const form = reactive({
+  nombre: '',
+  email: '',
+  mensaje: ''
+});
+
+
+emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+
+
+const sendEmail = () => {
+  const templateParams = {
+    subject: 'Mensaje de Portfolio',
+    name: form.nombre,
+    from_name: form.email,
+    message: form.mensaje
+  };
+
+  emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, templateParams)
+    .then(response => {
+      console.log('SUCCESS!', response.status, response.text);
+      alert('Correo enviado con éxito.');
+
+      form.nombre = '';
+      form.email = '';
+      form.mensaje = '';
+
+    })
+    .catch(error => {
+      console.log('FAILED...', error);
+      alert('Hubo un problema al enviar el correo.');
+    });
+};
+</script>
+
+
 <style>
-.logoContacto{
-  width: 5%;
+.logoContacto {
+  width: 50px;
 }
-</style>
+.contacto {
+  width: 80%;
+  height: 90%;
+  background-color: yellow;
+/*  background-color: red;*/
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.contenidoContacto{
+  height: 100%;
+  background-color: aquamarine;
+  display: flex;
+  flex-direction: column;
+
+
+}
+.apartadoContacto{
+  height: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  input, textarea{
+    font-family: 'Roboto', sans-serif;
+    font-size: 30px;
+    min-width: 200px;
+    width: 100%;
+    border-radius: 10%;
+    height: 30px;
+    resize: none;
+
+  }
+}
+.logosContactos{
+  height: 20%;
+}
+</style>  
