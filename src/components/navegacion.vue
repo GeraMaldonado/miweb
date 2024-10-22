714118bd1086e0035154ff32c0f960a86a574dc6
GeraMaldonado <gmaldonadofelix@gmail.com>
2024-10-21 20:43:31 -0600

    Se agregó el curriculum

--- src/components/navegacion.vue
+++ src/components/navegacion.vue
@@ -7,7 +7,7 @@
       <li><a href="#proyectos" :style="{ color: colorValue3 }">Proyectos</a></li>
       <li><a href="#contacto" :style="{ color: colorValue4 }">Contacto</a></li>
     </ul>
-    <a class="curriculum" v-show="menuVisible || !isMobile" href="../assets/curriculum.pdf">CURRICULUM</a>
+    <a class="curriculum" v-show="menuVisible || !isMobile" href="/Curriculum.pdf" download="Curriculum.pdf">Curriculum</a>
   </nav>
 </template>
 <script setup>
